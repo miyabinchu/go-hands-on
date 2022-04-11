d1f83f01b1e3ab4727b8eae6563c3b2f52a910c9
hgtm <masato.naganaga1@gmail.com>
2022-04-11 22:27:49 +0900

    add input()

--- hello.go
+++ hello.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, Go-lang!")
+	name := input("type your name")
+	fmt.Println("Hello, " + name + "!!")
+}
+
+func input(msg string) string {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(msg + ": ")
+	scanner.Scan()
+	return scanner.Text()
 }
